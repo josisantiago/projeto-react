99181354e6bfe209b96caf6a31a049fb9f2fd049
Josiane Santiago <josianesborrego@gmail.com>
2019-12-18 20:03:27 -0300

    iniciado projeto de estudos em react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+import Header from "./components/Header.js";
+
+const App = () => (
+   <div className="App">
+    <Header />
+    </div>
+);
+
+export default App;
+
+
