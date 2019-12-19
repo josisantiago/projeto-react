041cb534e257ecba3b53556e1d90e5259394863d
Josiane Santiago <josianesborrego@gmail.com>
2019-12-19 20:22:37 -0300

    feita header, requisição de api e estilização

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Component } from "react"; 
 
 import "./styles.css";
 
+import api from "./services/api";
+
 import Header from "./components/Header.js";
+
+import Main from "./pages/main";
 
 const App = () => (
    <div className="App">
     <Header />
+    <Main />
     </div>
 );
 
